77848ec598a4b97e8ed48da6f788b7e620734b82
JuicyRibs <moji_001122@hotmail.com>
2020-11-03 20:33:27 +0700

    ➕ Class page

---
+++ web/components/Meme.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="section">
+    <div class="columns is-justify-content-center">
+      <div class="column has-text-centered">
+        <img src="../assets/images/cat.jpg" />
+      </div>
+      <div class="column">
+        <h1>Polite cat</h1>
+        <h1>Cute eh</h1>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column is-4">
+        <img src="../assets/images/cat.jpg" />
+      </div>
+      <div class="column is-4">
+        <img src="../assets/images/cat.jpg" />
+      </div>
+      <div class="column is-4">
+        <img src="../assets/images/cat.jpg" />
+      </div>
+    </div>
+  </section>
+</template>
