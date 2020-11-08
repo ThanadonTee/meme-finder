bd8dbc37d9ee4038f266faff3ac6c5c44288f100
JuicyRibs <moji_001122@hotmail.com>
2020-11-08 22:50:12 +0700

    ➕Dynamic meme render
    connects to dataset and then wallah

--- web/components/MemeList.vue
+++ web/components/MemeList.vue
@@ -1,39 +1,17 @@
 <template>
-<section class="section">
-    <div class="columns">
-    <div class="columns">
-      <div class="column is-6">
-        <img src="~/assets/images/cat.jpg" />
-      </div>
-      <div class="column">
-        <h1 class="is-size-4">{meme.name}</h1>
-        <h1 class="is-size-5">{meme.desc}</h1>
-      </div>
-    </div>
-    <div class="columns">
-      <div class="column is-6">
-        <img src="~/assets/images/cat.jpg" />
-      </div>
-      <div class="column">
-        <h1 class="is-size-4">{meme.name}</h1>
-        <h1 class="is-size-5">{meme.desc}</h1>
-      </div>
-    </div>
-
-    <!-- <div class="column is-4" v-for="i in (memes.length/2)">
-
-          <div class="columns" v-for="meme in memes" :meme="meme">
-            <div class="column is-8">
-              <img :src="{meme.img_path}" />
-            </div>
-            <div class="column">
-              <h1 class="is-size-4">{meme.name}</h1>
-              <h1 class="is-size-5">{meme.desc}</h1>
-            </div>
+  <div>
+    <template v-for="i in 2">
+      <div class="columns">
+        <div class="columns" v-for="i in 2">
+          <div class="column">
+            <img src="~/assets/images/cat.jpg" />
           </div>
-
-        </div> -->
+          <div class="column">
+            <h1 class="is-size-4">Polite cat variation</h1>
+            <h1 class="is-size-5">Still cute eh</h1>
+          </div>
+        </div>
+      </div>
+    </template>
   </div>
-</section>
-  
 </template>
