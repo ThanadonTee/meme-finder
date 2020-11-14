975a7257a609783bfc3be344c723ceebdd82e6e4
JuicyRibs <moji_001122@hotmail.com>
2020-11-14 20:07:21 +0700

    ➕[WIP] Modal in meme list

--- web/components/MemeList.vue
+++ web/components/MemeList.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="mx-5">
     <template v-for="i in 2">
       <div class="columns">
         <div class="columns" v-for="i in 2">
-          <div class="column">
+          <div class="column" v-on:click="modalImage">
             <img src="~/assets/images/cat.jpg" />
           </div>
           <div class="column">
@@ -12,6 +12,33 @@
           </div>
         </div>
       </div>
+      
     </template>
+    <div class="modal" id="modalImage">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+          <p class="image">
+            <img
+              src="https://bulma.io/images/placeholders/1280x960.png"
+              alt=""
+            />
+          </p>
+        </div>
+        <button class="modal-close is-large" aria-label="close" v-on:click="closeModalImg"></button>
+      </div>
   </div>
 </template>
+
+
+<script>
+export default {
+  methods: {
+    modalImg: function() {
+          document.getElementsById("modalImage").classList.add("is-active");
+    },
+    closeModalImg: function() {
+          document.getElementsById("modalImage").classList.remove("is-active");
+    }
+  }
+}
+</script>
