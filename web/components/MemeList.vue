75484c35c1d0984192017b995df22ac6a6878e23
Chantisa <chanti.bodyslam@gmail.com>
2020-11-23 01:29:34 +0700

    finish all frontend

--- web/components/MemeList.vue
+++ web/components/MemeList.vue
@@ -3,12 +3,20 @@
     <template v-for="i in 2">
       <div class="columns">
         <div class="columns" v-for="i in 2">
-          <div class="column">
-            <img src="~/assets/images/cat.jpg" />
+          <div class="column is-3 is-centered">
+            <img src="~/assets/images/cat.jpg" width="300px" />
           </div>
           <div class="column">
             <h1 class="is-size-4">Polite cat variation</h1>
             <h1 class="is-size-5">Still cute eh</h1>
+            <div class="content">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                ornare magna eros, eu pellentesque tortor vestibulum ut.
+                Maecenas non massa sem. Etiam finibus odio quis feugiat
+                facilisis.
+              </p>
+            </div>
           </div>
         </div>
       </div>
