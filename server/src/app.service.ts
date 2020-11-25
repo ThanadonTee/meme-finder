ab85502f5a57a2ee447c215c29de34f920c369a0
Thanadon Teeratadpong <xXminningXx@gmail.com>
2020-11-25 17:48:23 +0700

    :wrench: hopefully remove refernce

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,19 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { execSync } from 'child_process';
 import { unlinkSync, writeFileSync } from 'fs';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const MEME = require('../AI/memes.json');
 @Injectable()
 export class AppService {
   getId(id: string): Record<string, unknown> {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const MEME = require('../AI/memes.json');
-
     const found = MEME.find(x => x.id == id);
     return { output: found };
   }
   postSearch(query: string): Record<string, unknown> {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const MEME = require('../AI/memes.json');
-
     const found = [];
     MEME.forEach(element => {
       element.generated_memes.every(el => {
@@ -26,9 +22,6 @@ export class AppService {
     return { output: found };
   }
   postImage(file): Record<string, unknown> {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    let MEME = require('../AI/memes.json');
-    MEME = Object.freeze(MEME);
     const filename = Date.now() + file.originalname;
     writeFileSync('./AI/uploads/' + filename, file.buffer);
     let result = execSync('./AI/env/bin/python3 AI/meme_finder.py ' + filename);
@@ -38,8 +31,8 @@ export class AppService {
     const top9 = result.slice(result.length - 9, result.length);
     const output = [];
     top9.forEach(el => {
-      let found = MEME.find(x => x.id == el[0]);
-      found = Reflect.deleteProperty(found, 'prop');
+      const found = JSON.parse(JSON.stringify(MEME.find(x => x.id == el[0])));
+      delete found.generated_memes;
       output.push(found);
     });
 
